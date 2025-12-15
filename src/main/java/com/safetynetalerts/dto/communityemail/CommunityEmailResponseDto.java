00d06c1180dcd4c2a8a8a6529f0cc6ae29c158d4
WebisBrian <brian.t.github@gmail.com>
2025-12-15 20:59:56 +0100

    Feature: add Dto

---
+++ src/main/java/com/safetynetalerts/dto/communityemail/CommunityEmailResponseDto.java
@@ -0,0 +1,34 @@
+package com.safetynetalerts.dto.communityemail;
+
+import java.util.List;
+
+public class CommunityEmailResponseDto {
+
+    // fields
+    private List<String> emails;
+
+    // constructors
+    public CommunityEmailResponseDto() {}
+
+    public CommunityEmailResponseDto(List<String> emails) {
+        this.emails = emails;
+    }
+
+    // getters and setters
+    public List<String> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<String> emails) {
+        this.emails = emails;
+    }
+
+    // overrides
+
+    @Override
+    public String toString() {
+        return "CommunityEmailResponseDto{" +
+                "emails=" + emails +
+                '}';
+    }
+}
