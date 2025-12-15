d9ab8ce924d36aa294475c0cf40544ecbb5ace3d
WebisBrian <brian.t.github@gmail.com>
2025-12-15 16:45:47 +0100

    Feature: TDD FloodController

---
+++ src/test/java/com/safetynetalerts/controller/FloodControllerTest.java
@@ -0,0 +1,51 @@
+package com.safetynetalerts.controller;
+
+import com.safetynetalerts.dto.flood.FloodAddressDto;
+import com.safetynetalerts.dto.flood.FloodResidentDto;
+import com.safetynetalerts.service.FloodService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(FloodController.class)
+class FloodControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    FloodService floodService;
+
+    @Test
+    void getFloodInfo_shouldReturnAddressesAndResidents() throws Exception {
+
+        FloodResidentDto resident = new FloodResidentDto(
+                "John", "Boyd", "841-874-6512", 41,
+                List.of("aznol:350mg"), List.of("nillacilan")
+        );
+
+        FloodAddressDto addressDto =
+                new FloodAddressDto("1509 Culver St", List.of(resident));
+
+        when(floodService.getFloodInfoByStations(List.of(1)))
+                .thenReturn(List.of(addressDto));
+
+        mockMvc.perform(get("/flood/stations").param("stations", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].address").value("1509 Culver St"))
+                .andExpect(jsonPath("$[0].residents[0].firstName").value("John"));
+    }
+}
