c4074afe1ccbcfda8a9240e763f05f2006ce7f49
WebisBrian <brian.t.github@gmail.com>
2026-01-07 09:53:30 +0100

    Suppress useless dependency

--- src/main/java/com/safetynetalerts/service/PhoneAlertServiceImpl.java
+++ src/main/java/com/safetynetalerts/service/PhoneAlertServiceImpl.java
@@ -11,7 +11,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class PhoneAlertServiceImpl implements PhoneAlertService {
